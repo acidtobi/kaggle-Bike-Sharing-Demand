7ae7d3bf54815411eef1f198a5bcd158e42b18ee
TL <tobi@rhabarber.(none)>
2014-06-05 20:55:18 +0200

    fixes

--- bikesharing.R
+++ bikesharing.R
@@ -10,24 +10,24 @@ train$weekday <- as.factor(strftime(train$datetime, "%A"))
 train$hour <- as.numeric(strftime(train$datetime, "%H"))
 
 all_days = data.frame(day = as.factor(seq(as.Date(train$datetime[1]), as.Date(train$datetime[nrow(train)]), by="day")))
+
+avg_rentals_per_day <- aggregate(count~day, train, mean)
+avg_rentals_per_weekday <- aggregate(count~weekday, train, mean)
+avg_rentals_per_workday <- aggregate(count~day, train[train$workingday==1,], mean)
+avg_rentals_per_weekday_and_hour <- aggregate(count~weekday+hour, train, mean)
+
 m <- merge(all_days, avg_rentals_per_day, by="day", all.x=T)
 m$count_approx <- na.spline(m$count)
 t <- m$count_approx
 plot(stl(ts(t,frequency=7),s.window="periodic",t.window = 3*19, t.jump=1, s.jump=1, na.action=na.exclude))
 
-rentals_per_weekday <- aggregate(count~weekday, train, mean)
-
-rentals_per_weekday_and_hour <- aggregate(count~weekday+hour, train, mean)
-agg <- rentals_per_weekday_and_hour
+agg <- avg_rentals_per_weekday_and_hour
 ggplot(data=agg, aes(x=agg$hour, y=agg$count, colour=as.factor(agg$weekday))) +  geom_line(size=1.5) 
 
-avg_rentals_per_day <- aggregate(count~day, train, mean)
-avg_rentals_per_workday <- aggregate(count~day, train[train$workingday==1,], mean)
-
 rentals_per_day <- aggregate(count~day+weekday, train, sum)
-ggplot(data=rentals_per_day, aes(x=rentals_per_day$day, y=rentals_per_day$count, colour=as.factor(rentals_per_day$weekday))) +  geom_line(size=1.5) 
+ggplot(data=rentals_per_day, aes(x=as.numeric(rentals_per_day$day), y=rentals_per_day$count, colour=rentals_per_day$weekday)) +  geom_line(size=1.5) 
 
 t <- train[train$weekday=="Monday" & train$holiday=="0",]
 ggplot(data=t, aes(x=t$hour, y=t$count, colour=as.factor(as.character(t$day)))) +  geom_line()
 
-ggplot(data=train, aes(x=train$datetime, y=train$temperature)) +  geom_line()
+ggplot(data=train, aes(x=train$datetime, y=train$temp)) +  geom_line()
